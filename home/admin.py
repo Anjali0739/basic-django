216ca807bc88d7bd085e60006d658120375c9fb6
Anjali0739 <anjalisoni0739@gmail.com>
2025-06-27 22:31:21 +0530

    Customized admin interface with status badges, inline editing, and filters

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Task
+from django.utils.html import format_html
 
 
 # Register your models here.
@@ -8,9 +9,18 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'completed', 'created')
+    list_display = ('title', 'status_badge', 'completed', 'created')
+    list_display_links = ('title',)
+    list_editable = ('completed',)
     list_filter = ('completed',)
     search_fields = ('title',)
+    ordering = ('-created',)
 
+    def status_badge(self, obj):
+        if obj.completed:
+            return format_html('<span style="color: green;">✅ Done</span>')
+        return format_html('<span style="color: red;">❌ Pending</span>')
+
+    status_badge.short_description = 'Status'
 
 
